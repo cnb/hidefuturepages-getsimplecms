d833af1b7c32687b85b2e6b6cb458d08f99b1a48
cnb <carnav@gmail.com>
2015-03-19 14:03:15 +0100

    version 0.2 - fixes #1 (sitemap)

--- hidefuturepages.php
+++ hidefuturepages.php
@@ -3,20 +3,21 @@
 $thisfile = basename(__FILE__, ".php");
 register_plugin($thisfile,
   'Hide future pages',
-  '0.1',
+  '0.2',
   'Carlos Navarro',
   'http://www.cyberiada.org/cnb/',
-  'Protects pages with future date from viewing, excludes them from I18N Search results', '', ''
+  'Protects pages with future date from viewing, excludes them from I18N Search results and Sitemap', '', ''
 );
 
 add_filter('search-veto','hidefuturepages_search'); // hide in I18N Search results
+add_filter('sitemap','hidefuturepages_sitemap'); // exclude from Sitemap
 add_action('index-pretemplate','hidefuturepages_view'); // page visibility
 
 function hidefuturepages_view() {
   global $private;
   if ($private != 'Y') {
     global $date;
-    if ( intval(strtotime($date)) >= intval(strtotime(date('r'))) ) {
+    if (intval(strtotime($date)) >= intval(strtotime(date('r')))) {
       global $USR;
       if (($USR) && $USR == get_cookie('GS_ADMIN_USERNAME')) {
         //ok, allow the person to see it then
@@ -28,7 +29,20 @@ function hidefuturepages_view() {
 }
 
 function hidefuturepages_search($item) {
-  return ( intval($item->pubDate) >= intval(strtotime(date('r'))) );
+  return intval($item->pubDate) >= intval(strtotime(date('r')));
+}
+
+function hidefuturepages_sitemap($sitemap){	
+  global $pagesArray;
+  foreach ($pagesArray as $page) {
+    if (intval(strtotime($page['pubDate'])) >= intval(strtotime(date('r')))) {
+      $url = find_url($page['slug'], $page['parent']);
+      foreach($sitemap->xpath("url/loc[.='".$url."']/parent::*") as $node){ 
+        unset($node[0]);
+      }
+    }
+	}
+	return $sitemap;
 }
 
 // end of file
